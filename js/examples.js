c7316b146003c037ec89286448155631ba105cf7
mehmetakifakkus <mehmetakifakkus@gmail.com>
2017-08-15 18:15:34 +0300

    minor changes

--- js/examples.js
+++ js/examples.js
@@ -1,12 +1,12 @@
 exampleCodes  = [
 	{
       "name": "3 sayının ortalamasını alan kod",
-      "description": "Well know Convex Hull problem is coded here. It is very basic algorithm in Computational Geometry."
-    },{
-      "name": "Smallest Enclosing Disk",
-      "link": "/algorithms/smallestEnclosingDisk",
-      "image": "images/smallestDisk_tn.png",
-      "description": "Problem of finding smallest circle that contains all of a given set of points in the Euclidean plane."
+      "description": 'Öğretmeniz bize bir sınav yapsın. Sınavdan önce şöyle bir şey desin: "80 den fazla not alanları ödüllendireceğim ve 10 puan ekstra vereceğim, altında alan her kişiden ise ceza amaçlı 5 puan kıracağım." Kişinin son puan durumunu hesaplayan kodu yazalım.',
+	  "code": "değişken puan = 75\n\neğer(puan > 80)\n	puan = puan + 10\ndeğilse\n	puan = puan - 5\n\nyaz puan\n"
+    },	{
+      "name": "Yurt dışından yapılan alışverişin tutarı",
+      "description": 'Yurtdışından dolar para birimi üzerinden satılan bir kaç ürün almak isteyelim. Bunların TL karşılığında neye tekabül ettiğini bulalım. Tabiki doların TL karşılığını değişken değeri olarak vereceğiz. Almak istediğimiz ürünlerin de dolar cinsinden tutarına sahibiz.',
+	  "code": "değişken dolarKuru = 3.45\ndeğişken saatFiyati = 60\ndeğişken telefonFiyati = 300\ndeğişken toplam = saatFiyati * dolarKuru + telefonFiyati * dolarKuru\n\nyaz toplam\n"
     },{
       "name": "L-monotone",
       "link": "/algorithms/L-monoton",
