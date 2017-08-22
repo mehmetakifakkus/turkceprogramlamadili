d9c8768f692e4af29252944069386536f396e16d
mehmetakifakkus <mehmetakifakkus@gmail.com>
2017-08-22 08:39:45 +0300

    örneklere bazı açıkamalar

--- js/examples.js
+++ js/examples.js
@@ -1,19 +1,19 @@
 beginner  = [
 	{
       "name": "3 sayının ortalamasını alma",
-      "description": '3 kişinin sahip olduğu paranın ortalamasını bulalım.',
+      "description": '3 kişinin sahip olduğu paranın ortalamasını bulalım.<br><br>',
 	  "code": "değişken para1 = 35\ndeğişken para2 = 45\ndeğişken para3 = 25\n\ndeğişken ort = (para1 + para2 + para3) / 3\n\nyaz ort"
     },{
       "name": "Yurt dışından yapılan alışverişin tutarı",
-      "description": 'Yurtdışından dolar para birimi üzerinden satılan bir kaç ürün almak isteyelim. Bunların TL karşılığında neye tekabül ettiğini bulalım. Tabiki doların TL karşılığını değişken değeri olarak vereceğiz. Almak istediğimiz ürünlerin de dolar cinsinden tutarına sahibiz.',
+      "description": 'Yurtdışından dolar para birimi üzerinden satılan bir kaç ürün almak isteyelim. Bunların TL karşılığında neye tekabül ettiğini bulalım. Tabiki doların TL karşılığını değişken değeri olarak vereceğiz. Almak istediğimiz ürünlerin de dolar cinsinden tutarına sahibiz.<br><br>',
 	  "code": "değişken dolarKuru = 3.45\ndeğişken saatFiyati = 60\ndeğişken telefonFiyati = 300\ndeğişken toplam = (saatFiyati + telefonFiyati) dolarKuru\n\nyaz toplam"
     },{
       "name": "Celcius sıcaklık birimini Fahrenheit'e çevirme",
-      "description": 'Celcius sıcaklık birimini Fahrenheit sıcaklık birimine çeviren kodu yazalım.',
+      "description": 'Celcius sıcaklık birimini Fahrenheit sıcaklık birimine çeviren kodu yazalım.<br><br>',
 	  "code": "değişken celcius = 30\n\ndeğişken fahrenheit = celcius * 1.8 + 32\n\nyaz fahrenheit"
     },{
       "name": "Dairenin çevresi ve alanı",
-      "description": 'Yarıçapı bilinen bir dairenin çevresini ve alanını hesaplayalım.',
+      "description": 'Yarıçapı bilinen bir dairenin çevresini ve alanını hesaplayalım.<br><br>',
 	  "code": "değişken r = 40\n"+
 		      "değişken pi = 3.14\n"+
 		      "\n"+
@@ -24,7 +24,7 @@ beginner  = [
 		      "yaz daire_çevresi\n"
     },{
       "name": "Silindir alanı ve hacmi",
-      "description": 'Taban yarıçapı ve yüksekliği verilmiş olan silindirin alanını ve hacmini hesaplayınız.',
+      "description": 'Taban yarıçapı ve yüksekliği verilmiş olan silindirin alanını ve hacmini hesaplayınız.<br><br>',
 	  "code": "değişken h = 5\n"+
 		      "değişken r = 40\n"+
 		      "değişken pi = 3.14\n"+
@@ -37,22 +37,22 @@ beginner  = [
     }
 	,{
       "name": "Arabanın ne yaktığını bulma",
-      "description": 'Kendi arabamız ya da aile arabamızın ne kadar yaktığını hesaplayalım. Varsayalım ki babanız sizden böyle bir program yazmanızı istedi. <br>Genel yaklaşım -ya da algoritma diyelim- şu şekilde olabilir: <br>Bir miktar yol katederiz, bunun bir şekilde kaydını tutarız, o sırada ne kadar tutarda yakıt harcadığımızı tespit ederiz. Örneğin, 236 km yol aldık ve 75 TL yakıt harcadık. <br><br> Hadi 1 km yolu ne kadarlık yakıtla katettiğimizi bulalım.',
+      "description": 'Kendi arabamız ya da aile arabamızın ne kadar yaktığını hesaplayalım. Varsayalım ki babanız sizden böyle bir program yazmanızı istedi. <br><br>Genel yaklaşım -ya da algoritma diyelim- şu şekilde olabilir: <br>Bir miktar yol katederiz, bunun bir şekilde kaydını tutarız, o sırada ne kadar tutarda yakıt harcadığımızı tespit ederiz. Örneğin, 236 km yol aldık ve 75 TL yakıt harcadık. <br><br> Hadi 1 km yolu ne kadarlık yakıtla katettiğimizi bulalım.<br><br>',
 	  "code": "değişken toplamYol = 236\ndeğişken toplamTutar = 75\ndeğişken fiyat = toplamTutar / toplamYol\n\nyaz fiyat"
     },
 	{
       "name": "Ehliyet almaya yeterli misin?",
-      "description": 'Ehliyet kursu yönetiyoruz. Öğrenciler bize ehliyet alabilmek için başvuruyor. Kodumuz ehliyet başvuru sisteminde çalışıyor varsayalım. Kişinin yaş bilgisine göre, "ehliyet alabilirsiniz" ya da "yaşınız ehliyet almaya yeterli değildir" gibi durum mesajlarını yazdırmak istiyoruz.',
+      "description": 'Ehliyet kursu yönetiyoruz. Öğrenciler bize ehliyet alabilmek için başvuruyor. Kodumuz ehliyet başvuru sisteminde çalışıyor varsayalım. Kişinin yaş bilgisine göre, "ehliyet alabilirsiniz" ya da "yaşınız ehliyet almaya yeterli değildir" gibi durum mesajlarını yazdırmak istiyoruz.<br><br>',
 	  "code": 'değişken yaş = 19\n\neğer(yaş > 18)\n	yaz "Ehliyet alabilirsiniz"\ndeğilse\n	yaz "Yaşınız ehliyet almaya yeterli değildir"'
     },
 	{
       "name": "Öğrencinin notunu hesaplama",
-      "description": 'Öğretmeniz bize bir sınav yapsın. Sınavdan önce şöyle bir şey desin: "80 den fazla not alanları ödüllendireceğim ve 10 puan ekstra vereceğim, altında alan her kişiden ise ceza amaçlı 5 puan kıracağım." Kişinin son puan durumunu hesaplayan kodu yazalım.',
+      "description": 'Öğretmeniz bize bir sınav yapsın. Sınavdan önce şöyle bir şey desin: <br><br>"80 den fazla not alanları ödüllendireceğim ve 10 puan ekstra vereceğim, altında alan her kişiden ise ceza amaçlı 5 puan kıracağım." Kişinin son puan durumunu hesaplayan kodu yazalım.<br><br>',
 	  "code": "değişken puan = 75\n\neğer(puan > 80)\n	puan = puan + 10\ndeğilse\n	puan = puan - 5\n\nyaz puan"
     },
 	{
       "name": "3 sayıdan en büyüğünü bulalım",
-      "description": 'Birbirlerine kıyasla başlangış değerlerini bilmediğimiz 3 sayı arasından en büyüğünü bulan programı geliştirelim.',
+      "description": 'Birbirlerine kıyasla başlangış değerlerini bilmediğimiz 3 sayı arasından en büyüğünü bulan programı geliştirelim.<br><br>',
 	  "code": "değişken num1 = 12\ndeğişken num2 = 10\ndeğişken num3 = 15\n\ndeğişken no = 0\n\neğer(num1 > no)\n	no = num1\n\neğer(num2 > no)\n	no = num2\n\neğer(num3 > no)\n	no = num3\n\nyaz no"
     },
 	{
@@ -86,7 +86,7 @@ ortaSeviye = [
 	},
 	{
 		"name": "Faktoriyel bulma 2",
-        "description": 'Bize verilen bir sayının faktoriyelini bulalım. Bu sefer birazcık farklı bir yöntem izliyoruz.',
+        "description": 'Bize verilen bir sayının faktoriyelini bulalım. Bu sefer birazcık farklı bir yöntem izliyoruz.<br><br>',
 		"code": "değişken sayı = 5   // faktöriyeli bulunacak değer\n"+
 				"değişken sonuç = 1  // sonuç burada saklanacak\n"+
 				"değişken sayaç = 1\n"+
@@ -110,12 +110,19 @@ ortaSeviye = [
 	                   "2, 6<br>"+
 	                   "2, 4<br>"+
 	                   "2, 2<br><br>"+
-	                   "İşte son durum, yani sayıların eşitlendiği durum bizim en büyük ortak bölenimizi veriyor. <br>",
+	                   "İşte son durum, yani sayıların eşitlendiği durum bizim en büyük ortak bölenimizi veriyor. <br><br>",
 		"code": "değişken num1 = 12\ndeğişken num2 = 6\n\nyinele(num1 != num2)\n{\n  eğer(num1 > num2)\n    num1 = num1 - num2\n  değilse\n    num2 = num2 - num1\n}\n\nyaz num1"
 	},
 	{
 		"name": "10 luk -> 2 lik sayı sistemi",
-        "description": '10 luk sayı sistemini 2 lik sayı sistemine çeviren kodu yazınız',
+        "description": '10 luk sayı sistemini 2 lik sayı sistemine çeviren kodu yazınız<br><br>İkilik tabanda ise şu şekilde ifade edilir:<br><br>'+
+						'<pre>'+
+						'1   1   1   0<br>'+
+						'|   |   |   |<br>'+
+						'|   |   |   |<br>'+
+						'v   v   v   v<br>'+
+						'2^3 2^2 2^1 2^0 = 13</pre><br>'+
+						'Bizim kodumuz ise bunun tam tersini yapacak<br><br>',
 		"code": "değişken sayı = 5\n"+
 				"değişken kalan = 0\n"+
                 "\n"+
